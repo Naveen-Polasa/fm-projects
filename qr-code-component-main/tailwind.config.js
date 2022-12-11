4282011d4bc5c428f727330ebe93335ae28824ef
Naveen Polasa <naveenpolasa99@gmail.com>
2022-12-11 22:50:33 +0530

    update files

---
+++ qr-code-component-main/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {},
+    fontFamily: {},
+    colors: {
+      bgc: "hsl(212, 45%, 89%)",
+      bgw: "hsl(0, 0%, 100%)",
+      db: "hsl(218, 44%, 22%)",
+    },
+  },
+  plugins: [],
+};
